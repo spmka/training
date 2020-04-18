7254a14806e1be2cb9d405351e87046060b8ba8c
spmka <61907809+spmka@users.noreply.github.com>
2020-04-18 13:03:03 +0200

    Update logistic-systems-end-points.controller.ts

--- step04/server/logistic-systems/logistic-systems-end-points.controller.ts
+++ step04/server/logistic-systems/logistic-systems-end-points.controller.ts
@@ -7,25 +7,29 @@ export class LogisticSystemsEndPointsController extends AbstractEndPointControll
 
   /** Set up our logistic systems end points */
   protected setupEndPoints() {
-    // We return the JSON object (array of logistic systems)
-    this.expressRouter.get('/', (req: Request, res: Response) => {
-      res.send(logisticSystems);
-    });
+    this.expressRouter.get('/', (req: Request, res: Response) => this.handleGetAll(res));
+    this.expressRouter.get('/:id', (req: Request, res: Response) => this.handleGetById(res, +req.params.id));
+  }
 
-    // Now get a parameter (:id) and we return the system with that id 
-    // or the error 404 NOT FOUND when we don't have a system with the given id
-    this.expressRouter.get('/:id', (req: Request, res: Response) => {
-      // We get the value for the parameter "id" from the req.params object
-      const id = req.params.id;
-      // We try to find the matching system in the systems array with the array function find()
-      const systemFound = logisticSystems.find(system => system.id === parseInt(id));
-      if (systemFound) {
-        // Ok we found a system so we return it
-        res.send(systemFound);
-      } else {
-        // We didn't find a system so we return a 404 and a message object
-        res.status(404).send({message: `No system with id ${id} found!`})
-      }
-    });
+  /**
+   * Handles a get request for all systems. 
+   * @param response the http response of the http get call.
+   */
+  private handleGetAll(response: Response) {
+    response.send(logisticSystems);
+  }
+
+  /**
+   * Handles a get request for one systems.
+   * @param response the http response of the http get call.
+   * @param systemId the system id the caller looks for.
+   */
+  private handleGetById(response: Response, systemId: number) {
+    const systemFound = logisticSystems.find(system => system.id === systemId);
+    if (systemFound) {
+      response.send(systemFound);
+    } else {
+      response.status(404).send({ message: `No system with id ${systemId} found!` })
+    }
   }
 }
