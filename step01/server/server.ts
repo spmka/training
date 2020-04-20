b4857e6bfbc87ce225b18bc33b4d28fcf4966ed9
spmka <61907809+spmka@users.noreply.github.com>
2020-04-20 17:11:42 +0200

    Update server.ts

--- step01/server/server.ts
+++ step01/server/server.ts
@@ -1,1 +1,20 @@
-console.log("Hello Wordl!");
+class HelloWorld {
+
+  /** Greets all. */
+  public greetAll() {
+    console.log('Hello world!');
+  }
+
+  /**
+   * Greets a given name.
+   * @param name who is to greet?
+   */
+  public greetOne(name: string) {
+    console.log('Hello ' + name);
+    console.log(`Hello ${name}`);
+  }
+}
+
+const helloWorld: HelloWorld = new HelloWorld();
+helloWorld.greetAll();
+helloWorld.greetOne('Andreas');
