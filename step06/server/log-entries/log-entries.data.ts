d14c4e756050ddc0a179d5381dd555df37645400
spmka <61907809+spmka@users.noreply.github.com>
2020-04-20 11:24:13 +0200

    Add files via upload

---
+++ step06/server/log-entries/log-entries.data.ts
@@ -0,0 +1,20 @@
+import {LogEntryList, LogLevel, logLevels} from './log-entry.model';
+import {logisticSystems} from '../logistic-systems/logistic-systems.data';
+import * as Chance from 'chance';
+
+const chance = new Chance();
+
+chance.mixin({
+  logEntry: () => {
+    const logLevel: LogLevel = chance.pickone(logLevels);
+    return {
+      systemId: chance.pickone(logisticSystems).id,
+      timestamp: chance.date({min: new Date(2020, 1, 1), max: new Date(2020, 6, 30)}),
+      level: logLevel,
+      message: logLevel + ' Message'
+    }
+  }
+});
+
+/** Some log entries to play with */
+export const logEntries: LogEntryList = chance.n((chance as any).logEntry, 1000);
