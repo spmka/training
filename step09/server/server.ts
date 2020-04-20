6762496e302490e571b5c0d4911c605156e385f9
spmka <61907809+spmka@users.noreply.github.com>
2020-04-20 11:21:20 +0200

    Add files via upload

---
+++ step09/server/server.ts
@@ -0,0 +1,36 @@
+import {AbstractExpressServer} from './abstract-express-server';
+import {LogEntriesEndPointsController} from './log-entries/log-entries.end-points.controller';
+import {LogisticSystemsEndPointsController} from './logistic-systems/logistic-systems-end-points.controller';
+import {HardwareUsageEndPointsController} from './hardware-usages/hardware-usage-end-point.controller';
+
+
+/** The control tower data server */
+class Server extends AbstractExpressServer {
+  /** The base url for the logistic systems end points */
+  private readonly logisticSystemsBaseUrl: string = '/logistic-systems';
+  /** The base url for the log entries end points */
+  private readonly logEntriesBaseUrl: string = '/log-entries';
+  /** The base url for the hardware usage end points */
+  private readonly hardwareUsagesBaseUrl: string = '/hardware-usages';
+
+  /**
+   * Constructor.
+   * @param port The port the control tower server listens on.
+   */
+  constructor(port: number) {
+    super(port);
+  }
+
+  /** Sets up all endpoints (routes) we want to use */
+  protected setupEndPoints() {
+    this.expressServer.use(this.logisticSystemsBaseUrl,
+      new LogisticSystemsEndPointsController().initializeEndPoints());
+    this.expressServer.use(this.logEntriesBaseUrl,
+      new LogEntriesEndPointsController().initializeEndPoints());
+    this.expressServer.use(this.hardwareUsagesBaseUrl,
+      new HardwareUsageEndPointsController().initializeEndPoints());
+  }
+}
+
+// We create the server and start it.
+new Server(5000).start();
