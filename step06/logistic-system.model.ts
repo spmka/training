e8488260c3d0ef929bc18728b61b6b5bec0ed799
spmka <61907809+spmka@users.noreply.github.com>
2020-04-20 11:23:11 +0200

    Delete logistic-system.model.ts

--- step06/logistic-system.model.ts
+++
@@ -1,9 +0,0 @@
-/** Describes a logistic system */
-export interface LogisticSystem {
-  id: number;
-  name: string;
-}
-
-/** A list of logistic systems */
-export type LogisticSystemList = Array<LogisticSystem>;
-// export type LogisticSystemList = LogisticSystem[];
