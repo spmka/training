0458a6c09c4269155c0a1d201383bba5c9047dbf
spmka <61907809+spmka@users.noreply.github.com>
2020-04-20 11:21:48 +0200

    Add files via upload

---
+++ step10/server/logistic-systems/logistic-systems.data.ts
@@ -0,0 +1,13 @@
+import {LogisticSystemList} from './logistic-system.model';
+
+/** All our logistic system we want to monitor */
+export const logisticSystems: LogisticSystemList = [
+  {id: 1, name: 'Logistikbus'},
+  {id: 2, name: 'ELS'},
+  {id: 3, name: 'WMS'},
+  {id: 4, name: 'LIBS'},
+  {id: 5, name: 'VDS2'},
+  {id: 6, name: 'VAS2000'},
+  {id: 7, name: 'PRIMUS'},
+  {id: 8, name: 'IPS'}
+];
